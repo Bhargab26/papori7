3d1644de6303fc6ad8a18c7e80cdaf2684a3f822
Bhargab kalita <bhargabkalita141@gmail.com>
2021-04-20 14:11:11 +0530

    mmm

--- site/Wish/challange/js/index.js
+++ site/Wish/challange/js/index.js
@@ -1,1 +1,29 @@
 // ⟁ \\
+using System.Collections;
+ using System.Collections.Generic;
+ using UnityEngine.UI;
+ using UnityEngine;
+ 
+ 
+ public class Button_Fade_In : MonoBehaviour
+ {
+     [SerializeField]
+     private float _sec;
+     public GameObject _objectToActivate;
+    
+ 
+     private void Start()
+     {
+         if (gameObject.activeInHierarchy)
+             gameObject.SetActive(false);
+ 
+         StartCoroutine(ActivationRoutine());
+     }
+ 
+     private IEnumerator ActivationRoutine()
+     {
+ 
+         yield return new WaitForSeconds(_sec * Time.deltaTime);
+ 
+         gameObject.SetActive(true);
+     }
